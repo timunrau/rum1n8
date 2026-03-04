534628eca07255798354f44a42e98b7206bf6724
Tim Unrau <timu@churchrenewal.com>
2026-03-03 20:48:26 -0600

    fix google login

--- vite.config.js
+++ vite.config.js
@@ -86,6 +86,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallbackDenylist: [/^\/gdrive-callback\.html/, /^\/privacy/],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
