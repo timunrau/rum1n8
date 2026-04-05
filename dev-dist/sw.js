bf9412a549baae4634e9cc1a063959db2ecd2127
Tim Unrau <timu@churchrenewal.com>
2026-04-05 08:27:18 -0500

    #31

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l7q8rjjnr9g"
+    "revision": "0.73cgp306gf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
